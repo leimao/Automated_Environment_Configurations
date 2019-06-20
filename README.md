95992ed0fd33b6bb117becad176d58e64ce9c445
Lei Mao <dukeleimao@gmail.com>
2019-06-19 23:02:41 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Automated_Environment_Configurations
+# Automated Environment Configurations
+
+Lei Mao
+
+## Introduction
+
+This repository is a collection of environment configuration bash scripts for fresh systems. The bash scripts could be used for Dockerfiles and cloud VM instances to get better setup reproducibility and efficiency. All the scripts have been tested on Google Cloud Platform (GCP). They should also work well on Amazon Web Services (AWS), Microsoft Azure Cloud, and other cloud service providers.
+
+
---
+++ Ubuntu-18.04-LTS/NVIDIA-Docker/nvidia_docker.sh
@@ -0,0 +1,32 @@
+# Upgrade Ubuntu Components
+sudo apt update
+sudo apt upgrade -y
+
+# NVIDIA driver and CUDA
+cd /tmp/
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.1.168-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804_10.1.168-1_amd64.deb
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install -y cuda
+
+# Docker
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+# sudo reboot
+
+# NVIDIA Docker
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+
+# Shutdown VM instance
+sudo poweroff
